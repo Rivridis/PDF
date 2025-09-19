c98a1ff2ea773dbe5d76051bd50c78b45924123a
Rivridis <sanjaypugal.personal@gmail.com>
2025-09-19 15:40:51 +0530

    added input

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
+from logging import root
+import tkinter as tk
 import pytesseract
 from PIL import Image, ImageDraw, ImageFont
 import fitz
+
+Field = ""
+Value = ""
+
+def save_file():
+    global Field, Value
+    Field = entry_field.get()
+    Value = entry_value.get()
+    root.after(100, root.destroy)
+
+root = tk.Tk()
+root.title("PDF Form")
+root.geometry("400x250")
+
+
+label_field = tk.Label(root, text="Field To Fill:")
+label_field.pack(pady=5)
+
+
+entry_field = tk.Entry(root, width=40)
+entry_field.pack(pady=5)
+
+label_value = tk.Label(root, text="Value:")
+label_value.pack(pady=5)
+
+entry_value = tk.Entry(root, width=40)
+entry_value.pack(pady=5)
+
+btn_enter = tk.Button(root, text="Enter", command=save_file)
+btn_enter.pack(pady=10)
+
+root.mainloop()
 
 pages = fitz.open("form.pdf")
 image = pages[0]
@@ -17,7 +51,7 @@ box = None
 
 for i in positions['text']:
     ind = positions['text'].index(i)
-    if "name" in i.lower() or "company" in i.lower():
+    if str(Field).lower() in i.lower():
         print(i)
         left, top, width, height = (positions['left'][ind],
                                     positions['top'][ind],
@@ -32,7 +66,7 @@ for i in positions['text']:
             # write on image
             draw = ImageDraw.Draw(img)
             font = ImageFont.truetype("arial.ttf", 40)
-            draw.text((pdf_x, pdf_y), "Rivridis", fill="black", font=font)
+            draw.text((pdf_x, pdf_y), str(Value), fill="black", font=font)
 
 
 
